791a5f4355efb262bd9a71a82d5db51c281cf4f9
CUHP <phuc260706@gmail.com>
2026-09-13 02:58:14 +0700

    chore: remove all temporary python scripts and data dumps

--- fix_inline_font.py
+++
@@ -1,8 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-html = html.replace("font-family:'Outfit'", "font-family:'Inter', sans-serif")
-html = html.replace("font-family:inherit", "font-family:'Inter', sans-serif")
-
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(html)
